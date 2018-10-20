30939d74829abecd1ee0a2b330e2686a7e3e92bf
Angie Kassab <anasab12@gmail.com>
2018-10-20 21:46:31 +0000

    added to menu in cli.rb

--- lib/harry_potter_books/cli.rb
+++ lib/harry_potter_books/cli.rb
@@ -3,12 +3,12 @@
 class HarryPotterBooks::CLI
 
   def call
-    puts "    Here are a list of Harry Potter Books:"
     list_books
     menu
   end
 
   def list_books
+    puts "Here are a list of Harry Potter Books:"
     puts <<-DOC
     1. Harry Potter and the Philosopher's Stone
     2. Harry Potter and the Chamber of Secrets
@@ -23,6 +23,25 @@ class HarryPotterBooks::CLI
 
   def menu
     puts "Enter the number of the book you'd like a summary about:"
+    input = gets.strip
+    case input
+    when "1"
+      puts "Here is a summary of Harry Potter and the Philosopher's Stone:"
+    when "2"
+      puts "Here is a summary of Harry Potter and the Chamber of Secrets:"
+    when "3"
+      puts "Here is a summary of Harry Potter and the Prisoner of Azkaban:"
+    when "4"
+      puts "Here is a summary of Harry Potter and the Goblet of Fire:"
+    when "5"
+      puts "Here is a summary of Harry Potter and the Order of the Phoenix:"
+    when "6"
+      puts "Here is a summary of Harry Potter and the Half-Blood Prince:"
+    when "7"
+      puts "Here is a summary of Harry Potter and the Deathly Hallows:"
+    when "8"
+      puts  "Here is a summary of Harry Potter and the Cursed Child- Parts One and Two playscript:"
+    end
   end
 
 end
