889ae836e682787db6bf569c84e030deae5c7ed9
AngieAngelica <38296176+AngieAngelica@users.noreply.github.com>
2018-10-20 15:42:45 -0400

    Update harry_potter_books.rb

--- lib/harry_potter_books.rb
+++ lib/harry_potter_books.rb
@@ -1,5 +1,8 @@
-require "harry_potter_books/version"
+#This file is going to require all my other files
+require_relative "./harry_potter_books/version"
 
 module HarryPotterBooks
   # Your code goes here...
 end
+
+require_relative "./harry_potter_books/cli"
