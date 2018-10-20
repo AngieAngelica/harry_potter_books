65fb638e4deae9634d905a013ba484d79d6c63f6
Angie Kassab <anasab12@gmail.com>
2018-10-20 20:28:18 +0000

    moved files around

--- lib/harry_potter_books.rb
+++ lib/harry_potter_books.rb
@@ -1,3 +1,8 @@
 #This file is going to require all my other files and acts as my environment
+
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
 require_relative "./harry_potter_books/version"
 require_relative "./harry_potter_books/cli"
