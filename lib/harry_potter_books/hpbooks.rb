3cfe3e2cb42faa546916c2477bf50fbd4b9bee19
Angie Kassab <anasab12@gmail.com>
2018-10-21 19:32:14 +0000

    created code for instantiating objects

--- lib/harry_potter_books/hpbooks.rb
+++ lib/harry_potter_books/hpbooks.rb
@@ -15,57 +15,91 @@ class HarryPotterBooks::Potter
 
   def self.scrape_book_1
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/3.Harry_Potter_and_the_Sorcerer_s_Stone"))
-    name = doc.css("h1").text
-    summary = doc.css("span")[47].inner_text
-    goodreads_rating = doc.css("span")[40].inner_text
-    quote = doc.css("span")[-3].text
+
+    book_1 = self.new
+    book_1.name = doc.css("h1").text
+    book_1.summary = doc.css("span")[47].inner_text
+    book_1.goodreads_rating = doc.css("span")[40].inner_text
+    book_1.quote = doc.css("span")[-3].text
+
+    book_1
+
   end
-binding.pry
+
   def self.scrape_book_2
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/15881.Harry_Potter_and_the_Chamber_of_Secrets"))
-    name = doc.css("h1").text # same as book 1
-    summary = doc.css("span")[47].inner_text # same as book 1
-    goodreads_rating = doc.search("span.average").text #different from book 1
-    quote = doc.css("span")[-3].text
+
+    book_2 = self.new
+    book_2.name = doc.css("h1").text # same as book 1
+    book_2.summary = doc.css("span")[47].inner_text # same as book 1
+    book_2.goodreads_rating = doc.search("span.average").text #different from book 1
+    book_2.quote = doc.css("span")[-3].text
+
+    book_2
+
   end
 
   def self.scrape_book_3
-    doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/5.Harry_Potter_and_the_Prisoner_of_Azkaban"))
-    name = doc.css("h1").text # same as book 2
-    summary = doc.css("span")[47].inner_text # same as book 2
-    goodreads_rating = doc.search("span.average").text #same as book 2
-    quote = doc.css("span")[-3].text
+    doc = Nokogiri::HTML(open("https://www.goodreads.com/book/show/5.Harry_Potter_and_the_Prisoner_of_Azkaban"))
+
+    book_3 = self.new
+    book_3.name = doc.css("h1").text # same as book 2
+    book_3.summary = doc.css("span")[47].inner_text # same as book 2
+    book_3.goodreads_rating = doc.search("span.average").text #same as book 2
+    book_3.quote = doc.css("span")[-3].text
+
+    book_3
+
   end
 
   def self.scrape_book_4
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/6.Harry_Potter_and_the_Goblet_of_Fire"))
-    name = doc.css("h1").text # same as book 2 and 3
-    summary = doc.css("span")[47].inner_text # same as book 2 and 3
-    goodreads_rating = doc.search("span.average").text #same as book 2 and 3
-    quote = doc.css("span")[-3].text
+
+    book_4 = self.new
+    book_4.name = doc.css("h1").text # same as book 2 and 3
+    book_4.summary = doc.css("span")[47].inner_text # same as book 2 and 3
+    book_4.goodreads_rating = doc.search("span.average").text #same as book 2 and 3
+    book_4.quote = doc.css("span")[-3].text
+
+    book_4
+
   end
 
   def self.scrape_book_5
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/2.Harry_Potter_and_the_Order_of_the_Phoenix"))
-    name = doc.css("h1").text # same as 2, 3,and 4
-    summary = doc.css("span")[47].inner_text # same as 2, 3,and 4
-    goodreads_rating = doc.search("span.average").text # same as 2, 3,and 4
-    quote = doc.css("span")[-3].text
+
+    book_5 = self.new
+    book_5.name = doc.css("h1").text # same as 2, 3,and 4
+    book_5.summary = doc.css("span")[47].inner_text # same as 2, 3,and 4
+    book_5.goodreads_rating = doc.search("span.average").text # same as 2, 3,and 4
+    book_5.quote = doc.css("span")[-3].text
+
+    book_5
   end
 
   def self.scrape_book_6
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/1.Harry_Potter_and_the_Half_Blood_Prince"))
-    name = doc.css("h1").text # same
-    summary = doc.css("span")[47].inner_text # same
-    goodreads_rating = doc.search("span.average").text # same
-    quote = doc.css("span")[-3].text
+
+    book_6 = self.new
+    book_6.name = doc.css("h1").text # same
+    book_6.summary = doc.css("span")[47].inner_text # same
+    book_6.goodreads_rating = doc.search("span.average").text # same
+    book_6.quote = doc.css("span")[-3].text
+
+    book_6
+
   end
 
   def self.scrape_book_7
     doc= Nokogiri::HTML(open("https://www.goodreads.com/book/show/136251.Harry_Potter_and_the_Deathly_Hallows"))
-    name = doc.css("h1").text # same
-    summary = doc.css("span")[47].inner_text
-    goodreads_rating = doc.search("span.average").text
-    quote = doc.css("span")[-3].text
+
+    book_7 = self.new
+    book_7.name = doc.css("h1").text # same
+    book_7.summary = doc.css("span")[47].inner_text
+    book_7.goodreads_rating = doc.search("span.average").text
+    book_7.quote = doc.css("span")[-3].text
+
+    book_7
+
   end
 end
